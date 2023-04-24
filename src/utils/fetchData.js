22fb68b42bb60fd095f06a2be8f1fa34f4494265
John Doe <mikiyasfiseha097@gmail.com>
2023-04-23 17:10:58 -0700

    search

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd64baf57eamsh554f4bc1a8bf3c4p1ef2d4jsn95f6c6e0b747',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd64baf57eamsh554f4bc1a8bf3c4p1ef2d4jsn95f6c6e0b747',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
